03c46f97e0d99ed8274e8efbd982c299d7f84438
ac-bryan12 <bpalava@espol.edu.ec>
2021-10-15 22:25:11 -0500

    Creation of getters and setters

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,16 +2,44 @@ package ec.edu.espol.workshops;
 
 public class CarInsurance {
 
-	public Integer age;
-	public char genre;
-	public String marital_status;
+	private Integer age;
+	private char genre;
+	private String maritalStatus;
 	
 	public CarInsurance() {}
 	
-	public CarInsurance(Integer age, char genre, String marital_status){
+	public CarInsurance(Integer age, char genre, String maritalStatus){
 		this.age = age;
 		this.genre = genre;
-		this.marital_status = marital_status;
+		this.maritalStatus = maritalStatus;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public char getGenre() {
+		return this.genre;
+	} 
+	
+	public void setGenre(String genre) {
+		if (genre.toUpperCase().equals("F") || genre.toUpperCase().equals("M")) {
+			this.genre =  genre.toUpperCase().charAt(0); 			
+		}
+	}
+	
+	public String getMaritalStatus() {
+		return this.maritalStatus;
+	} 
+	
+	public void setMaritalStatus(String maritalStatus) {
+		if(maritalStatus.toUpperCase().equals("MARRIED") || maritalStatus.toUpperCase().equals("NOT_MARRIED") ) {
+			this.maritalStatus = maritalStatus.toUpperCase();
+		}
 	}
 	
 }
