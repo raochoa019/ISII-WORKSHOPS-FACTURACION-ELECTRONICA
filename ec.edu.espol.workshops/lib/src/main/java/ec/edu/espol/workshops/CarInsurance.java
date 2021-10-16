8d85ffe83986ccda3e5154e8f4ec1e77d933f6f9
raochoa019 <raochoa@espol.edu.ec>
2021-10-15 21:42:02 -0500

    Creation of Class CarInsurance

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -0,0 +1,17 @@
+package ec.edu.espol.workshops;
+
+public class CarInsurance {
+
+	public Integer age;
+	public char genre;
+	public String marital_status;
+	
+	public CarInsurance() {}
+	
+	public CarInsurance(Integer age, char genre, String marital_status){
+		this.age = age;
+		this.genre = genre;
+		this.marital_status = marital_status;
+	}
+	
+}
