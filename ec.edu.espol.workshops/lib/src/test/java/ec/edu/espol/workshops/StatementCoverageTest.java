308331fb1ceaab55055904c76fe5db2b3cbf264f
Bryan Alava <bpalava@espol.edu.ec>
2022-01-02 22:25:54 -0500

    Test cases for statement coverage were created

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StatementCoverageTest.java
@@ -8,9 +8,21 @@ import org.junit.jupiter.api.Test;
 class StatementCoverageTest {
 
 	@Test
-	void test() {
+	void tcs1() {
+		int esperado = 2000;
+		CarInsurance c = new CarInsurance(18,'M',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	void tcs2() {
 		int esperado = 200;
-		CarInsurance c = new CarInsurance(50,'M',"MARRIED");
+		CarInsurance c = new CarInsurance(50,'F',"MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	void tcs3() {
+		int esperado = -1;
+		CarInsurance c = new CarInsurance(86,'M',"MARRIED");
 		int valor = c.calculatedPremiumCar(); 
 		assertEquals(esperado,valor,0);
 	}
