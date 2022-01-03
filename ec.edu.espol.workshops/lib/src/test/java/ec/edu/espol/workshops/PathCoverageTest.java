2d31bb962f9a727871262e9fd9d234202e1a60cc
avillao <villaoaron@gmail.com>
2022-01-03 18:44:54 -0500

    Path testing cases

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/PathCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/PathCoverageTest.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.workshops;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,59 @@ import org.junit.jupiter.api.Test;
 class PathCoverageTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void tcp1() {
+		int esperado = 500;
+		CarInsurance c = new CarInsurance(30,'M',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp2() {
+		int esperado = 2000;
+		CarInsurance c = new CarInsurance(24,'M',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp3() {
+		int esperado = 300;
+		CarInsurance c = new CarInsurance(30,'F',"MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp4() {
+		int esperado = 400;
+		CarInsurance c = new CarInsurance(50,'M',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp5() {
+		int esperado = -1;
+		CarInsurance c = new CarInsurance(86,'M',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp6() {
+		int esperado = 200;
+		CarInsurance c = new CarInsurance(50,'M',"MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void tcp7() {
+		int esperado = -1;
+		CarInsurance c = new CarInsurance(86,'M',"MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
 	}
 
 }
