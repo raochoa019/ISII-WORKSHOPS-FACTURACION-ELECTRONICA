8c83e8c809d64c272b7ee62152dc0f2f3e71b781
avillao <villaoaron@gmail.com>
2022-01-20 23:45:04 -0500

    Test cases for new features

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsuranceTest.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsuranceTest.java
@@ -11,11 +11,10 @@ import org.junit.jupiter.api.Test;
 
 class CarInsuranceTest {
 
-
 	@Test
 	void test1CalculatedPremiumCar() {
-		int esperado = 200;
-		CarInsurance c = new CarInsurance(50,'M',"MARRIED");
+		int esperado = 500;
+		CarInsurance c = new CarInsurance(30,'M',"NOT MARRIED");
 		int valor = c.calculatedPremiumCar(); 
 		assertEquals(esperado,valor,0);
 	}
@@ -23,13 +22,30 @@ class CarInsuranceTest {
 	@Test
 	void test2CalculatedPremiumCar() {
 		int esperado = 300;
+		CarInsurance c = new CarInsurance(25,'F',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	
+	@Test
+	void test3CalculatedPremiumCar() {
+		int esperado = 300;
 		CarInsurance c = new CarInsurance(65,'F',"MARRIED");
 		int valor = c.calculatedPremiumCar(); 
 		assertEquals(esperado,valor,0);
 	}
 	
 	@Test
-	void test3CalculatedPremiumCar() {
+	void test4CalculatedPremiumCar() {
+		int esperado = 200;
+		CarInsurance c = new CarInsurance(50,'F',"NOT MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
+	}
+	
+	@Test
+	void test5CalculatedPremiumCar() {
 		int esperado = 500;
 		CarInsurance c = new CarInsurance(65,'M',"NOT MARRIED");
 		int valor = c.calculatedPremiumCar(); 
@@ -37,47 +53,29 @@ class CarInsuranceTest {
 	}
 	
 	@Test
-	void test4CalculatedPremiumCar() {
-		int esperado = 300;
-		CarInsurance c = new CarInsurance(25,'F',"NOT MARRIED");
-		int valor = c.calculatedPremiumCar(); 
-		assertEquals(esperado,valor,0);
-	}
-	
-	@Test
-	void test5CalculatedPremiumCar() {
-		int esperado = 300;
-		CarInsurance c = new CarInsurance(25,'f',"NOT MARRIED");
-		int valor = c.calculatedPremiumCar(); 
-		assertEquals(esperado,valor,0);
-	}
-	
-	@Test
 	void test6CalculatedPremiumCar() {
-		int esperado = 300;
-		CarInsurance c = new CarInsurance(25,'F',"Not married");
+		int esperado = 100;
+		CarInsurance c = new CarInsurance(50,'M',"MARRIED");
 		int valor = c.calculatedPremiumCar(); 
 		assertEquals(esperado,valor,0);
 	}
 	
 	@Test
 	void test7MaritalStatus() {
-		String esperado = "NOT MARRIED";
-		CarInsurance c = new CarInsurance(25,'F',"Not married");
-		String estado = c.getMaritalStatus();
-		System.out.println(esperado.equals(c.getMaritalStatus()));
-		assertEquals(estado,esperado);
+		int esperado = -1;
+		CarInsurance c = new CarInsurance(86,'M',"MARRIED");
+		int valor = c.calculatedPremiumCar(); 
+		assertEquals(esperado,valor,0);
 		
 	}
 	
 	@Test
 	void test8CalculatedPremiumCar() {
-		int esperado = 300;
-		CarInsurance c = new CarInsurance(35,'H',"married");
+		int esperado = 150;
+		CarInsurance c = new CarInsurance(62,'F',"NOT MARRIED");
 		int estado = c.calculatedPremiumCar();
 		assertEquals(esperado,estado,0); 
 	}
-	
 	
 	
 
